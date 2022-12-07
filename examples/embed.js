d65c214c237b7eb833a9f12d98a08266c0d383af
colder <cncolder@gmail.com>
2022-12-07 13:38:13 +0800

    chore: add examples

---
+++ examples/embed.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+exports.add = void 0;
+function add(a, b) {
+    return a + b;
+}
+exports.add = add;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZW1iZWRkZWQuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJlbWJlZGRlZC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7QUFBQSxTQUFnQixHQUFHLENBQUMsQ0FBUyxFQUFFLENBQVM7SUFDdEMsT0FBTyxDQUFDLEdBQUcsQ0FBQyxDQUFBO0FBQ2QsQ0FBQztBQUZELGtCQUVDIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGZ1bmN0aW9uIGFkZChhOiBudW1iZXIsIGI6IG51bWJlcikge1xuICByZXR1cm4gYSArIGJcbn1cbiJdfQ==
